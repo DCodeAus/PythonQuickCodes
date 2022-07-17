d5ca35ce1a4f3a08dbaab5896fa7aab704e865f5
DCodeAus <>
2022-07-17 22:44:10 +1000

    Intialised project

---
+++ QRCodeReader.py
@@ -0,0 +1,19 @@
+#pre requsitie
+#pip install pyqrcode
+#pip install pypng
+
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+#string for the QR code
+S = "www.geeksforgeeks.org"
+
+#generate QR code
+url = pyqrcode.create(S)
+
+#create and save the svg file
+url.svg("PythonQuickCodes/myqr.svg", scale = 8 )
+
+#create and save the png file
+url.png("PythonQuickCodes/myqr.png", scale = 6)
